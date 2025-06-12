29745ea5e588c9cfaefb0b4b7d13a90360c26352
‪Mostafa Mohamed‬‏ <145389902+mostafaM99D@users.noreply.github.com>
2025-06-13 00:33:54 +0300

    initial commit

---
+++ SevenSeg/SevenSeg.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace SevenSeg
+{
+    partial class SevenSeg
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.btnTwo = new System.Windows.Forms.Button();
+            this.btnThree = new System.Windows.Forms.Button();
+            this.btnSix = new System.Windows.Forms.Button();
+            this.btnFive = new System.Windows.Forms.Button();
+            this.btnFour = new System.Windows.Forms.Button();
+            this.btnNine = new System.Windows.Forms.Button();
+            this.btnEight = new System.Windows.Forms.Button();
+            this.btnSeven = new System.Windows.Forms.Button();
+            this.btnZero = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Enabled = false;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn1.Location = new System.Drawing.Point(109, 16);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(235, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Enabled = false;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn2.Location = new System.Drawing.Point(331, 44);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(25, 198);
+            this.btn2.TabIndex = 2;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Enabled = false;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn4.Location = new System.Drawing.Point(99, 44);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(25, 198);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Enabled = false;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn3.Location = new System.Drawing.Point(109, 248);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(235, 23);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Enabled = false;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn5.Location = new System.Drawing.Point(331, 277);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(25, 198);
+            this.btn5.TabIndex = 5;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Enabled = false;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn7.Location = new System.Drawing.Point(99, 277);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(25, 198);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Enabled = false;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn6.Location = new System.Drawing.Point(109, 481);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(235, 23);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btnOne
+            // 
+            this.btnOne.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOne.Location = new System.Drawing.Point(109, 559);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(67, 39);
+            this.btnOne.TabIndex = 8;
+            this.btnOne.Text = "1";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // btnTwo
+            // 
+            this.btnTwo.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTwo.Location = new System.Drawing.Point(203, 559);
+            this.btnTwo.Name = "btnTwo";
+            this.btnTwo.Size = new System.Drawing.Size(67, 39);
+            this.btnTwo.TabIndex = 9;
+            this.btnTwo.Text = "2";
+            this.btnTwo.UseVisualStyleBackColor = true;
+            this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+            // 
+            // btnThree
+            // 
+            this.btnThree.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThree.Location = new System.Drawing.Point(295, 559);
+            this.btnThree.Name = "btnThree";
+            this.btnThree.Size = new System.Drawing.Size(67, 39);
+            this.btnThree.TabIndex = 10;
+            this.btnThree.Text = "3";
+            this.btnThree.UseVisualStyleBackColor = true;
+            this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+            // 
+            // btnSix
+            // 
+            this.btnSix.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSix.Location = new System.Drawing.Point(295, 624);
+            this.btnSix.Name = "btnSix";
+            this.btnSix.Size = new System.Drawing.Size(67, 39);
+            this.btnSix.TabIndex = 13;
+            this.btnSix.Text = "6";
+            this.btnSix.UseVisualStyleBackColor = true;
+            this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+            // 
+            // btnFive
+            // 
+            this.btnFive.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFive.Location = new System.Drawing.Point(203, 624);
+            this.btnFive.Name = "btnFive";
+            this.btnFive.Size = new System.Drawing.Size(67, 39);
+            this.btnFive.TabIndex = 12;
+            this.btnFive.Text = "5";
+            this.btnFive.UseVisualStyleBackColor = true;
+            this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+            // 
+            // btnFour
+            // 
+            this.btnFour.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFour.Location = new System.Drawing.Point(109, 624);
+            this.btnFour.Name = "btnFour";
+            this.btnFour.Size = new System.Drawing.Size(67, 39);
+            this.btnFour.TabIndex = 11;
+            this.btnFour.Text = "4";
+            this.btnFour.UseVisualStyleBackColor = true;
+            this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+            // 
+            // btnNine
+            // 
+            this.btnNine.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNine.Location = new System.Drawing.Point(295, 683);
+            this.btnNine.Name = "btnNine";
+            this.btnNine.Size = new System.Drawing.Size(67, 39);
+            this.btnNine.TabIndex = 16;
+            this.btnNine.Text = "9";
+            this.btnNine.UseVisualStyleBackColor = true;
+            this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+            // 
+            // btnEight
+            // 
+            this.btnEight.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEight.Location = new System.Drawing.Point(203, 683);
+            this.btnEight.Name = "btnEight";
+            this.btnEight.Size = new System.Drawing.Size(67, 39);
+            this.btnEight.TabIndex = 15;
+            this.btnEight.Text = "8";
+            this.btnEight.UseVisualStyleBackColor = true;
+            this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+            // 
+            // btnSeven
+            // 
+            this.btnSeven.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeven.Location = new System.Drawing.Point(109, 683);
+            this.btnSeven.Name = "btnSeven";
+            this.btnSeven.Size = new System.Drawing.Size(67, 39);
+            this.btnSeven.TabIndex = 14;
+            this.btnSeven.Text = "7";
+            this.btnSeven.UseVisualStyleBackColor = true;
+            this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+            // 
+            // btnZero
+            // 
+            this.btnZero.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZero.Location = new System.Drawing.Point(203, 753);
+            this.btnZero.Name = "btnZero";
+            this.btnZero.Size = new System.Drawing.Size(67, 39);
+            this.btnZero.TabIndex = 17;
+            this.btnZero.Text = "0";
+            this.btnZero.UseVisualStyleBackColor = true;
+            this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(109, 753);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(67, 39);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(490, 822);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnZero);
+            this.Controls.Add(this.btnNine);
+            this.Controls.Add(this.btnEight);
+            this.Controls.Add(this.btnSeven);
+            this.Controls.Add(this.btnSix);
+            this.Controls.Add(this.btnFive);
+            this.Controls.Add(this.btnFour);
+            this.Controls.Add(this.btnThree);
+            this.Controls.Add(this.btnTwo);
+            this.Controls.Add(this.btnOne);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SevenSeg";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.Button btnTwo;
+        private System.Windows.Forms.Button btnThree;
+        private System.Windows.Forms.Button btnSix;
+        private System.Windows.Forms.Button btnFive;
+        private System.Windows.Forms.Button btnFour;
+        private System.Windows.Forms.Button btnNine;
+        private System.Windows.Forms.Button btnEight;
+        private System.Windows.Forms.Button btnSeven;
+        private System.Windows.Forms.Button btnZero;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ SevenSeg/SevenSeg.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace SevenSeg
+{
+    public partial class SevenSeg : Form
+    {
+        public SevenSeg()
+        {
+            InitializeComponent();
+            
+            _Reset();
+        }
+        private void _Reset()
+        {
+            btn1.BackColor = Color.Black;
+            btn2.BackColor = Color.Black;
+            btn3.BackColor = Color.Black;
+            btn4.BackColor = Color.Black;
+            btn5.BackColor = Color.Black;
+            btn6.BackColor = Color.Black;
+            btn7.BackColor = Color.Black;
+        }
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn4.BackColor= Color.Red;
+            btn7.BackColor= Color.Red;
+        }
+
+        private void btnTwo_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn7.BackColor = Color.Red;
+            btn6.BackColor = Color.Red;
+        }
+
+        private void btnThree_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+            btn6.BackColor = Color.Red;
+        }
+
+        private void btnFour_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn4.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+        }
+
+        private void btnFive_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn4.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+            btn6.BackColor = Color.Red;
+        }
+
+        private void btnSix_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn4.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+            btn7.BackColor = Color.Red;
+            btn6.BackColor = Color.Red;
+        }
+
+        private void btnSeven_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+        }
+
+        private void btnEight_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn4.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+            btn7.BackColor = Color.Red;
+            btn6.BackColor = Color.Red;
+        }
+
+        private void btnNine_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn3.BackColor = Color.Red;
+            btn4.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+        }
+
+        private void btnZero_Click(object sender, EventArgs e)
+        {
+            _Reset();
+            btn1.BackColor = Color.Red;
+            btn2.BackColor = Color.Red;
+            btn4.BackColor = Color.Red;
+            btn5.BackColor = Color.Red;
+            btn7.BackColor = Color.Red;
+            btn6.BackColor = Color.Red;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _Reset();
+        }
+    }
+}
